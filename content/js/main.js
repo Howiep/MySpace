2c6350fd8691beedaf8fcd6705ff744436768ad9
Howie P <hawkarp@gmail.com>
2017-05-19 07:13:38 +0200

    no message

--- content/js/main.js
+++ content/js/main.js
@@ -2,31 +2,51 @@
 //   $('.dropdown-toggle', this).trigger('click');
 // });
 
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+$(document).ready(function() {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();
+    trigger.click(function() {
+        hamburger_cross();
     });
 
     function hamburger_cross() {
 
-      if (isClosed == true) {
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
 
-  $('[data-toggle="offcanvas"]').click(function () {
+    $('[data-toggle="offcanvas"]').click(function() {
         $('#wrapper').toggleClass('toggled');
-  });
+    });
 });
+(function() {
+    var grid;
+
+    function init() {
+        grid = new Minigrid({
+            container: '.cards',
+            item: '.card',
+            gutter: 6
+        });
+        grid.mount();
+    }
+
+    // mount
+    function update() {
+        grid.mount();
+    }
+
+    document.addEventListener('DOMContentLoaded', init);
+    window.addEventListener('resize', update);
+})();
