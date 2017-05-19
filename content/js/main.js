0cdc62e6c86cf3f6a01671183933b24f0fbb10f8
Howie <hawkarp@gmail.com>
2017-05-19 14:44:29 +0200

    pc

--- content/js/main.js
+++ content/js/main.js
@@ -30,23 +30,3 @@ $(document).ready(function() {
         $('#wrapper').toggleClass('toggled');
     });
 });
-(function() {
-    var grid;
-
-    function init() {
-        grid = new Minigrid({
-            container: '.cards',
-            item: '.card',
-            gutter: 6
-        });
-        grid.mount();
-    }
-
-    // mount
-    function update() {
-        grid.mount();
-    }
-
-    document.addEventListener('DOMContentLoaded', init);
-    window.addEventListener('resize', update);
-})();
